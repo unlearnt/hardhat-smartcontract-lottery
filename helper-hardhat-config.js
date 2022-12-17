f328db608a85dadcc048e9a8fb3178071b976aaf
unlearnt <unlearnt92@gmail.com>
2022-12-18 00:03:02 +0800

    config change

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -15,7 +15,7 @@ const networkConfig = {
     },
     5: {
         name: "goerli",
-        subscriptionId: "6926",
+        subscriptionId: "7692",
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
